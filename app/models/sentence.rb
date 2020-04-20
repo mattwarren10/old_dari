1df859383a0c9491e5841a03f23e31a04280b6b3
mattwarren10 <mwarren@harding.edu>
2020-04-20 18:16:40 -0400

    add relations to sentence and word models

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,4 +1,10 @@
 class Sentence < ApplicationRecord
+
+  #relations
+  belongs_to :word
+
+
+	#validations
 	validates_presence_of :english_sentence,
 												:transliteration_sentence,
 												:translation_sentence,
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,14 @@
 class Word < ApplicationRecord
+
+
+	#relations
+	has_many :sentences
+
+	#validations
 	validates_presence_of :english_word,
 												:tense,
 												:transliteration,
 												:translation,
 												:pronunciation
+
+end
