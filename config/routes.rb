6568da60382d8064df72e7c26163adfbd85f6c11
mattwarren10 <mwarren@harding.edu>
2020-04-21 22:02:48 -0400

    update routes to POST using the create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
-  get 'sentences/new'
-  get 'sentences/create'
-  get 'sentences/update'
-  get 'sentences/edit'
-  get 'sentences/delete'
-  get 'sentences/destroy'
-  get 'sentences/index'
-  get 'sentences/show'
-  get 'words/new'
-  get 'words/create'
-  get 'words/update'
-  get 'words/edit'
-  get 'words/destroy'
-  get 'words/index'
-  get 'words/show'
+  root :to => 'words#index'
+  post 'words/create' 
+  resources :words, only: [:new, :edit, :destroy, :index, :show] do
+    resources :sentences, only: [:new, :edit, :destroy, :index, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
