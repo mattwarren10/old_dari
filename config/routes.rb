cbab57d328b8424fc201c8c659d0a15cc8adabce
mattwarren10 <mwarren@harding.edu>
2020-04-23 20:40:57 -0400

    update routes to have words_update_path available

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root :to => 'words#index'
-  post 'words/create' 
-  resources :words, only: [:new, :edit, :destroy, :index, :show] do
-    resources :sentences, only: [:new, :edit, :destroy, :index, :show]
+  post 'words/create'
+  patch 'words/:id/update/', action: :update, controller: 'words', as: 'words_update'
+  resources :words, only: [:new, :edit, :destroy, :index, :show, :update] do
+    resources :sentences, only: [:new, :edit, :destroy, :index, :show] do
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
